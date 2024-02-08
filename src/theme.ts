659fba05a45122e25a2696c17212aca6dfd50439
siddhant agrawal <siddhant_010@outlook.com>
2024-02-08 23:54:53 +0530

    change the background color

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,20 @@ const config: ThemeConfig = {
     initialColorMode: "dark"
 };
 
-const theme  = extendTheme({config}); // calling the extend theme and pass the object with a confilct property. This returns a theme that you store here.   
+const theme  = extendTheme({config,
+colors:{
+    gray:{
+        50: '#f9f9f9',
+        100: '#ededed',
+        200: '#d3d3d3',
+        300: '#b3b3b3',
+        400: '#a0a0a0',
+        500: '#898989',
+        600: '#6c6c6c',
+        700: '#202020',
+        800: '#121212',
+        900: '#111',
+    }
+}}); // calling the extend theme and pass the object with a confilct property. This returns a theme that you store here.   
 
 export default theme;
